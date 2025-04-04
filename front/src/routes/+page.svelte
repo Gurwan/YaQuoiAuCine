8d833d4fa384d57e1b6a0afcade3c66507887428
Gurwan <34190539+Gurwan@users.noreply.github.com>
2025-04-04 18:50:05 +0200

    create home page

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="max-w-screen-2xl mx-auto px-4 mt-6">
+    <nav class="bg-gray-800 p-4">
+        <h1 class="logo">Y a quoi au ciné ?</h1>
+    </nav>
+</div>
+
+<img src="/home_no_bg.webp" alt="" class="absolute bottom-0 left-0 w-auto max-w-xs md:max-w-sm lg:max-w-md" />
+
+<style>
+    .logo {
+        background: url('/logo_texte.webp') no-repeat center;
+        background-size: contain;
+        width: 230px;
+        height: 85px;
+        text-indent: -9999px;
+        display: block;
+    }
+</style>
--- front/svelte.config.js
+++ front/svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
+	compilerOptions: {
+		runes: true
+	},
 	preprocess: vitePreprocess(),
 	kit: { adapter: adapter() }
 };
