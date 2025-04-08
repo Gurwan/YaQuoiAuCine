c67c87be50d1437765ddbe30ffd60c7e18ca224d
Gurwan <gurwandelaunay35@gmail.com>
2025-04-08 18:54:37 +0200

    add type Movie

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	import type { Movie } from '$lib/types/movie';
 	let data = $props();
-	let movies = data.data.movies;
+	let movies: Movie[] = data.data.movies;
 </script>
 
 <div class="mx-auto mt-6 max-w-screen-2xl px-4">
@@ -8,14 +9,14 @@
 		<h1 class="logo">Y a quoi au ciné ?</h1>
 	</nav>
 
-	{#if movies?.length == 0}
+	{#if movies?.length > 0}
 		<ul class="mt-3 space-y-4">
 			{#each movies as movie}
 				<li class="rounded bg-gray-100 p-4 shadow">
 					<h2 class="text-lg font-semibold">{movie.title}</h2>
-					{#if movie.poster_path}
+					{#if movie.poster}
 						<img
-							src={'https://image.tmdb.org/t/p/w300' + movie.poster_path}
+							src={'https://image.tmdb.org/t/p/w300' + movie.poster}
 							alt={movie.title}
 							class="mt-2 rounded"
 						/>
@@ -24,19 +25,17 @@
 			{/each}
 		</ul>
 	{:else}
-		<div class="mt-10 mb-20 flex items-center justify-center">
-			<img
-				src="loading.gif"
-				alt="Chargement..."
-				class="loading-gif"
-			/>
+		<div class="mb-20 mt-10 flex items-center justify-center">
+			<img src="loading.gif" alt="Chargement..." class="loading-gif" />
 		</div>
 	{/if}
 
-	<footer class="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
-		<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-			<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-			<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="/" class="hover:underline">Y a quoi au ciné™</a>. All Rights Reserved.</span>
+	<footer class="m-4 rounded-lg bg-white shadow-sm dark:bg-gray-900">
+		<div class="mx-auto w-full max-w-screen-xl p-4 md:py-8">
+			<hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
+			<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"
+				>© 2025 <a href="/" class="hover:underline">Y a quoi au ciné™</a>. All Rights Reserved.</span
+			>
 		</div>
 	</footer>
 </div>
--- front/src/routes/+page.ts
+++ front/src/routes/+page.ts
@@ -1,7 +1,8 @@
+import type { Movie } from '$lib/types/movie';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
     const res = await fetch('http://localhost:3000/movies');
-    const movies = await res.json();
+    const movies: Movie[] = await res.json();
     return { movies };
 };
