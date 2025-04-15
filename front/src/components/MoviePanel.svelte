46d182bc24bb92adf8d9e6caaf31226c87bfa9fa
Gurwan <gurwandelaunay35@gmail.com>
2025-04-15 18:42:13 +0200

    add empty movie panel

---
+++ front/src/components/MoviePanel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { MovieDetails } from "$lib/types/movie-details";
+
+	let data = $props();
+	let movie_details: MovieDetails = data;
+
+</script>
+
+
+
+</button>
+
+<style>
+
+</style>
